ecd022b4e1771780a3e614366f3265e92dc7b2e3
Mahesh7614 <waghmaremahesh012@gmail.com>
2022-11-20 11:27:24 +0530

    Permutations of String

---
+++ Algorithm_Programs/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Algorithm_Programs
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            UserInput input= new UserInput();
+            input.UserInputForPrograms();
+        }
+    }
+}
---
+++ Algorithm_Programs/UserInput.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Algorithm_Programs
+{
+    public class UserInput
+    {
+        public void UserInputForPrograms()
+        {
+            Console.WriteLine("Enter - 1. Permutation ");
+
+            Console.Write("Enter Selected Number : ");
+            int userInput = int.Parse(Console.ReadLine());
+
+            switch (userInput)
+            {
+                case 1:
+                    Console.WriteLine("\nEnter String for Permutation");
+                    string str = Console.ReadLine();
+                    int Length = str.Length;
+                    Permutations permutations = new Permutations();
+                    Console.WriteLine("Using Recursion Method Permutations are :: ");
+                    permutations.Recursionpermutation(str, 0, Length - 1); // using Recursion Menthod
+                    Console.WriteLine("Using Iterative Method Permutations are :: ");
+                    permutations.IterationPermutation(str); // using iterative Menthod
+                    break;
+            }
+        }
+    }
+}
